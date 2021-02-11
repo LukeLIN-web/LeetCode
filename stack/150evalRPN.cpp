ff9ea534957e3454a9d0512708f450f2de661582
LukeLIN-web <1263810658@qq.com>
2021-02-11 17:18:48 +0800

    stack RPN finished

--- stack/150evalRPN.cpp
+++ stack/150evalRPN.cpp
@@ -5,7 +5,25 @@ using namespace std;
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        
-
+        stack<int> st;
+        for(int i = 0; i < tokens.size();i++){
+            string s = tokens[i];
+            if(s == "+" ||  s == "-"  || s == "*" || s== "/"  ){
+                int a =  st.top();
+                st.pop();
+                int b  = st.top();
+                st.pop();
+                switch(s[0]){
+                    case '+' :  st.push( a+b); break;// switch 不能用string
+                    case '-' :   st.push( b-a);break;
+                    case '*' :   st.push( b*a);break;
+                    case '/' :  st.push( b/a);
+                }
+            }
+            else{
+                st.push(stoi(s));
+            }
+        }
+        return st.top();
     }
 };
