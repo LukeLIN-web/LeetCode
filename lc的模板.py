1bd01d1a481360191c64000bbe93343f1ce822c0
LukeLIN-web <1263810658@qq.com>
2021-07-31 20:58:37 +0800

    7.31

--- lc的模板.py
+++ lc的模板.py
@@ -1,8 +1,6 @@
 from functools import reduce
 from typing import List
-import numpy as np
 import numpy as np 
-from fuctools import reduce
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
 
--- 草稿纸.py
+++ 草稿纸.py
@@ -1,9 +1,18 @@
-from collections import deque
+from collections import Counter, deque
 from typing import List
 l = [[0,2,2],[4,2,4],[2,13,1000000000]]
-print(l[0][1:])
-# print (2 in dict.keys())
-# f = open('log.txt','w')
-# for i in range(1,101,1):
-#     print(str(i),end= ",", file=f)
-# f.close()
+# s ="2aaa"
+# print (Counter(s).values())
+# t = Solution()
+# print(t.maximumElementAfterDecrementingAndRearranging([100,1,1000]) )
+# def debug(func):
+#     def wrapper():
+#         print("[DEBUG]: enter {}()".format(func.__name__))
+#         return func()
+#     return wrapper
+
+# @debug
+# def hello():
+#     print("hello")
+
+# hello()
